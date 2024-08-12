8662427f9e4f4c36a62254eb860b821e46f58249
jc16082004 <143001678+jc16082004@users.noreply.github.com>
2024-08-12 07:53:00 -0500

    Add files via upload

---
+++ CORTE1/codigo1.py
@@ -0,0 +1,9 @@
+print("Mi primera vez con phyton")
+a = "cadena de caracteres"
+print("tipo dato a: ", type(a))
+b = 8
+print("tipo dato b: ", type(b))
+c = 3.1416
+print("tipo dato c: ", type(c))
+
+
