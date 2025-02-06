9f61c5c011a484a4e818b353e13aec4a38ba689b
jc16082004 <143001678+jc16082004@users.noreply.github.com>
2025-02-06 14:11:56 -0500

    Create diccionario2.py

---
+++ CORTE 2/diccionario2.py
@@ -0,0 +1,27 @@
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+print(building_heights["Burj Khalifa"]) # Prints 828
+print(building_heights["Ping An"]) # Prints 599
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+print(zodiac_elements["earth"])
+print(zodiac_elements["fire"])
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+print(building_heights["Landmark 81"])
+test_scores = {"Grace":[80, 72, 90], "Jeffrey":[88, 68, 81], "Sylvia":[80, 82, 84], "Pedro":[98, 96, 95], "Martin":[78, 80, 78], "Dina":[64, 60, 75]}
+print(list(test_scores))
+test_scores = {"Grace":[80, 72, 90], "Jeffrey":[88, 68, 81], "Sylvia":[80, 82, 84], "Pedro":[98, 96, 95], "Martin":[78, 80, 78], "Dina":[64, 60, 75]}
+for score_list in test_scores.values():
+  print(score_list)
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+total_exercises = 0
+for exercises in num_exercises.values():
+  total_exercises += exercises
+print(total_exercises)
+biggest_brands = {"Apple": 184, "Google": 141.7, "Microsoft": 80, "Coca-Cola": 69.7, "Amazon": 64.8}
+
+for company, value in biggest_brands.items():
+print(company + " has a value of " + str(value) + " billion dollars. ")
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for occupation, percentage in pct_women_in_occupation.items():
+  print("Women make up " + str(percentage) + " percent of " + occupation + "s.") 
